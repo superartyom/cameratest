076f27be78a1a19f897c7b5f49295ca30474c01f
superartyom <62669220+superartyom@users.noreply.github.com>
2023-05-05 20:43:01 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,77 @@
+import logging
+import queue
+from pathlib import Path
+
+import streamlit as st
+from aiortc.contrib.media import MediaPlayer
+
+from streamlit_webrtc import (
+    ClientSettings,
+    WebRtcMode,
+    webrtc_streamer,
+)
+
+HERE = Path(__file__).parent
+
+logger = logging.getLogger(__name__)
+
+
+WEBRTC_CLIENT_SETTINGS = ClientSettings(
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    media_stream_constraints={
+        "video": True,
+        "audio": True,
+    },
+)
+
+
+def main():
+    st.header("WebRTC demo")
+    app_sendonly_video()
+    
+
+
+def app_sendonly_video():
+    webrtc_ctx = webrtc_streamer(
+        key="video-sendonly",
+        mode=WebRtcMode.SENDONLY,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
+    )
+
+    image_place = st.empty()
+
+    while True:
+        if webrtc_ctx.video_receiver:
+            try:
+                video_frame = webrtc_ctx.video_receiver.get_frame(timeout=1)
+            except queue.Empty:
+                logger.warning("Queue is empty. Abort.")
+                break
+
+            img_rgb = video_frame.to_ndarray(format="rgb24")
+            image_place.image(img_rgb)
+        else:
+            logger.warning("AudioReciver is not set. Abort.")
+            break
+
+
+if __name__ == "__main__":
+    import os
+
+    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+        "%(message)s",
+        force=True,
+    )
+
+    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+
+    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+    st_webrtc_logger.setLevel(logging.DEBUG)
+
+    fsevents_logger = logging.getLogger("fsevents")
+    fsevents_logger.setLevel(logging.WARNING)
+
+    main()
